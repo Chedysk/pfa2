e147adf71973b16260b12f33e0234d25b5ac35cc
Chedysk <124107151+Chedysk@users.noreply.github.com>
2023-03-30 20:08:29 +0100

    Update TrainServiceImplTest.java

--- src/test/java/tn/esprit/spring/TrainServiceImplTest.java
+++ src/test/java/tn/esprit/spring/TrainServiceImplTest.java
@@ -4,7 +4,7 @@ package tn.esprit.spring;
 //import org.springframework.boot.test.context.SpringBootTest;
 //
 //@SpringBootTest
-//class ExamThourayaS2ApplicationTests {
+//class ExamThourayaS2ApplicationTests {a
 //
 //	@Test
 //	void contextLoads() {
